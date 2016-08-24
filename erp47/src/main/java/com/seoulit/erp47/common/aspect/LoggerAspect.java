490471151fee48ab4dd904b2d484be1f61928266
YonG <YonG@DESKTOP-IT68SDO>
2016-08-24 21:20:57 +0900

    Error

---
+++ erp47/src/main/java/com/seoulit/erp47/common/aspect/LoggerAspect.java
@@ -0,0 +1,25 @@
+package com.seoulit.erp47.common.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+//@Component
+//@Aspect
+public class LoggerAspect {
+    
+    public Object aroundLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+        String name = joinPoint.getSignature().getName();
+        String type = joinPoint.getSignature().getDeclaringTypeName();
+        
+        log.debug("{}.{}() Ω√¿€", type, name);
+        
+        Object returnValue = joinPoint.proceed();
+        
+        log.debug("{}.{}() ≥°", type, name);
+        
+        return returnValue;
+        
+    }
+}
