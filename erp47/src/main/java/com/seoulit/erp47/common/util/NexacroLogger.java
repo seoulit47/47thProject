7711e8c49d6f7ccd054eb60e4d2f1def374ffd62
YonG <YonG@DESKTOP-IT68SDO>
2016-08-25 19:52:10 +0900

    Error

--- erp47/src/main/java/com/seoulit/erp47/common/util/NexacroLogger.java
+++ erp47/src/main/java/com/seoulit/erp47/common/util/NexacroLogger.java
@@ -10,26 +10,26 @@ public class NexacroLogger {
     
     private final static Debugger debugger = new Debugger();
     
-    // 데이터셋 출력
+
     public static void debug(PlatformData platformData, String datasetName) {
         DataSet dataSet = platformData.getDataSet(datasetName);
         System.out.println(debugger.detail(dataSet));
     }
     
-    // 데이터셋, 인자리스트 모두 출력
+
     public static void debug(PlatformData platformData) {
         debug(platformData.getDataSetList());
         debug(platformData.getVariableList());
     }
     
-    // 데이터셋, 인자리스트 모두 출력
+
     public static void debug(DataSetList dataSetList, VariableList variableList) {
         debug(dataSetList);
         debug(variableList);
     }
     
 
-    // 데이터셋(dataset) 출력
+
     public static void debug(DataSetList dataSetList) {
         // DEBUG -> DataSetList
         int dataSetListCount = dataSetList.size();
@@ -38,7 +38,7 @@ public class NexacroLogger {
         }
     }
     
-    // 인자리스트(argument) 출력
+
     public static void debug(VariableList variableList) {
         int variableListCount = variableList.size();
         for (int index = 0; index < variableListCount; index++) {
