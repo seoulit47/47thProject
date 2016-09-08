7f753c05653edf60cf6c0a56ea0b54478b3f0f00
qhdwls915 <qhdwls915@gmail.com>
2016-09-08 21:33:55 +0900

    2016.09.08 by bong

--- erp47/src/main/java/com/seoulit/erp47/hrs/emp/controller/EmpController.java
+++ erp47/src/main/java/com/seoulit/erp47/hrs/emp/controller/EmpController.java
@@ -1,5 +1,6 @@
 package com.seoulit.erp47.hrs.emp.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,35 +14,101 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.nexacro.xapi.data.PlatformData;
 import com.seoulit.erp47.common.util.DataSetBeanMapper;
 import com.seoulit.erp47.hrs.emp.service.EmpServiceFacade;
+import com.seoulit.erp47.hrs.emp.to.CareerBean;
+import com.seoulit.erp47.hrs.emp.to.DisabilityBean;
+import com.seoulit.erp47.hrs.emp.to.EducaBean;
 import com.seoulit.erp47.hrs.emp.to.EmpBean;
+import com.seoulit.erp47.hrs.emp.to.FamilyBean;
+import com.seoulit.erp47.hrs.emp.to.LicenseBean;
+import com.seoulit.erp47.hrs.emp.to.MilitaryBean;
+import com.seoulit.erp47.hrs.emp.to.PerconaldateBean;
 
 /**
- * @Package  com.seoulit.erp47.com.base.controller
- * @Class    LoginController.java
- * @Create   2016. 08. 26.
- * @Author   Hwanee
+ * @Package com.seoulit.erp47.com.base.controller
+ * @Class EmpController.java
+ * @Create 2016. 08. 26.
+ * @Author Hwanee
  * @Description 사원 컨트롤러
- *  	
- * @LastUpdated 
+ * 
+ * @LastUpdated 사원정보 조회 : 2016.09.08 by 김봉진
  */
 
 @Controller
 public class EmpController {
 
-    @Autowired
-    private EmpServiceFacade empServiceFacade;
+	@Autowired
+	private EmpServiceFacade empServiceFacade;
 
-    @Autowired
-    private DataSetBeanMapper datasetBeanMapper;
+	@Autowired
+	private DataSetBeanMapper datasetBeanMapper;
 
-    @RequestMapping("hrs/emp/findEmpList.do")
-    public void findEmpList(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    	PlatformData inData=(PlatformData) request.getAttribute("inData");
-    	PlatformData outData=(PlatformData) request.getAttribute("outData");
-    	
-    	Map<String, String> argsMap  = datasetBeanMapper.variablesToMap(inData);
-    	
-    	List<EmpBean> empList=empServiceFacade.findEmpList(argsMap);
-    	datasetBeanMapper.beansToDataset(outData, empList, EmpBean.class);
-    }
+	@RequestMapping("hrs/emp/findEmpList.do")
+	public void findEmpList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		PlatformData inData = (PlatformData) request.getAttribute("inData");
+		PlatformData outData = (PlatformData) request.getAttribute("outData");
+
+		Map<String, String> argsMap = datasetBeanMapper.variablesToMap(inData);
+
+		List<EmpBean> empList = empServiceFacade.findEmpList(argsMap);
+		datasetBeanMapper.beansToDataset(outData, empList, EmpBean.class);
+	}
+
+	@RequestMapping("hrs/emp/selectEmpList.do")
+	public void selectEmpList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		PlatformData outData = (PlatformData) request.getAttribute("outData");
+		PlatformData inData = (PlatformData) request.getAttribute("inData");
+		Map<String, String> argsMap = datasetBeanMapper.variablesToMap(inData);
+		System.out.println("Check" + argsMap);
+		List<EmpBean> list = empServiceFacade.selectEmpList(argsMap);
+
+		List<CareerBean> careerList = new ArrayList<CareerBean>();
+		List<DisabilityBean> disabilityList = new ArrayList<DisabilityBean>();
+		List<EducaBean> educationList = new ArrayList<EducaBean>();
+		List<FamilyBean> familyList = new ArrayList<FamilyBean>();
+		List<LicenseBean> licenseList = new ArrayList<LicenseBean>();
+		List<MilitaryBean> militaryList = new ArrayList<MilitaryBean>();
+		List<PerconaldateBean> perconaldateList = new ArrayList<PerconaldateBean>();
+
+		for (EmpBean empBean : list) {
+
+			List<CareerBean> careerBeanList = empBean.getCareerList();
+			if (careerBeanList != null)
+				careerList.addAll(careerBeanList);
+			
+			List<DisabilityBean> disabilityBeanList = empBean.getDisabilityList();
+			if (disabilityBeanList != null)
+				disabilityList.addAll(disabilityBeanList);
+			
+			List<EducaBean> educationBeanList = empBean.getEducationBeanList();
+			if (educationBeanList != null)
+				educationList.addAll(educationBeanList);
+			
+			List<FamilyBean> familyBeanList = empBean.getFamilyList();
+			if (familyBeanList != null)
+				familyList.addAll(familyBeanList);
+			
+			List<LicenseBean> licenseBeanList = empBean.getLicenseList();
+			if (licenseBeanList != null)
+				licenseList.addAll(licenseBeanList);
+			
+			List<MilitaryBean> militaryBeanList = empBean.getMilitaryList();
+			if (militaryBeanList != null)
+				militaryList.addAll(militaryBeanList);
+			
+			List<PerconaldateBean> perconaldateBeanList = empBean.getPerconaldateList();
+			if (perconaldateBeanList != null)
+				perconaldateList.addAll(perconaldateBeanList);
+		}
+
+		datasetBeanMapper.beansToDataset(outData, list, EmpBean.class);
+
+		datasetBeanMapper.beansToDataset(outData, careerList, CareerBean.class);
+		datasetBeanMapper.beansToDataset(outData, disabilityList, DisabilityBean.class);
+		datasetBeanMapper.beansToDataset(outData, educationList, EducaBean.class);
+		datasetBeanMapper.beansToDataset(outData, familyList, FamilyBean.class);
+		datasetBeanMapper.beansToDataset(outData, licenseList, LicenseBean.class);
+		datasetBeanMapper.beansToDataset(outData, militaryList, MilitaryBean.class);
+		datasetBeanMapper.beansToDataset(outData, perconaldateList, PerconaldateBean.class);
+
+	}
 }
--- erp47/src/main/java/com/seoulit/erp47/hrs/emp/to/EmpBean.java
+++ erp47/src/main/java/com/seoulit/erp47/hrs/emp/to/EmpBean.java
@@ -1,6 +1,9 @@
 package com.seoulit.erp47.hrs.emp.to;
 
+import java.util.List;
+
 import com.seoulit.erp47.common.annotation.Dataset;
+import com.seoulit.erp47.common.annotation.Remove;
 import com.seoulit.erp47.common.to.BaseBean;
 
 import lombok.Getter;
@@ -24,8 +27,110 @@ import lombok.ToString;
 @Dataset(name = "gdsEmp")
 public class EmpBean extends BaseBean {
 
-	private String 	empNo,		//사원번호
-					empNm,		//사원명
-					password;	//비밀번호
+	List<CareerBean> careerList;			//경력리스트
+	List<DisabilityBean> disabilityList;	//장애리스트
+	List<EducaBean> educationBeanList;		//교육리스트
+	List<FamilyBean> familyList;			//가족리스트
+	List<LicenseBean> licenseList;			//자격리스트
+	List<MilitaryBean> militaryList;		//병역리스트
+	List<PerconaldateBean> perconaldateList;//인적사항리스트
+	
+	@Remove
+	public List<CareerBean> getCareerList() {
+		return careerList;
+	}
+	@Remove
+	public void setCareerList(List<CareerBean> careerList) {
+		this.careerList = careerList;
+	}
+	@Remove
+	public List<DisabilityBean> getDisabilityList() {
+		return disabilityList;
+	}
+	@Remove
+	public void setDisabilityList(List<DisabilityBean> disabilityList) {
+		this.disabilityList = disabilityList;
+	}
+	@Remove
+	public List<EducaBean> getEducationBeanList() {
+		return educationBeanList;
+	}
+	@Remove
+	public void setEducationBeanList(List<EducaBean> educationBeanList) {
+		this.educationBeanList = educationBeanList;
+	}
+	@Remove
+	public List<FamilyBean> getFamilyList() {
+		return familyList;
+	}
+	@Remove
+	public void setFamilyList(List<FamilyBean> familyList) {
+		this.familyList = familyList;
+	}
+	@Remove
+	public List<LicenseBean> getLicenseList() {
+		return licenseList;
+	}
+	@Remove
+	public void setLicenseList(List<LicenseBean> licenseList) {
+		this.licenseList = licenseList;
+	}
+	@Remove
+	public List<MilitaryBean> getMilitaryList() {
+		return militaryList;
+	}
+	@Remove
+	public void setMilitaryList(List<MilitaryBean> militaryList) {
+		this.militaryList = militaryList;
+	}
+	@Remove
+	public List<PerconaldateBean> getPerconaldateList() {
+		return perconaldateList;
+	}
+	@Remove
+	public void setPerconaldateList(List<PerconaldateBean> perconaldateList) {
+		this.perconaldateList = perconaldateList;
+	}
+	
+	String frignSingleTxrate, //외국인단일세율
+		   empNo,             //사원번호
+		   empNm,			  //사원명
+		   jobtt,			  //직위
+		   jobcl,			  //직종
+		   jobrk,		      //직급
+		   rrn,				  //주민등록번호
+		   birthday,		  //생년월일
+		   gender,			  //성별
+		   mateYn,			  //배우자유무
+		   addr,			  //주소
+		   zipCd,			  //우편번호
+		   permaddr,		  //기준등록지
+		   tel,				  //전화번호
+		   phone,			  //휴대폰번호
+		   extsn,			  //원내전화번호
+		   fax,				  //팩스번호
+		   password,		  //비밀번호
+		   image,		      //이미지파일명
+		   natnt,			  //국적
+		   localYn,			  //내외국인여부
+		   forinNum,		  //외국인등록번호
+		   deptCd,		      //부서코드
+		   hireDiv,		      //고용구분
+		   workStatus,		  //재직상태
+		   workPeriod,	      //근속년수
+		   retDate,	          //퇴사일
+		   retMonthWorkingdays,//퇴직예정일
+		   payStep,		      //호봉
+		   pms,				  //권한
+		   hireDate,		  //입사일
+		   detailAddr,		  //상세주소
+		   email,			  //이메일주소
+		   deptNm,			  //부서명
+		   bankNm,		      //은행명
+		   accountNm,		  //계좌번호
+		   payDate,           //급여일
+		   sts,				  //상태
+		   loginIp,           //최종접속ip
+		   loginTime;         //최종접속시간
 	
 }
