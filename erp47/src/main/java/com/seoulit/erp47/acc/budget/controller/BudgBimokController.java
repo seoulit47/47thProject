324687020e4443ff56f1982dd0640bee7f458258
seoulit47 <47members@naver.com>
2016-09-09 10:40:30 +0900

    9/9 10:40

--- erp47/src/main/java/com/seoulit/erp47/acc/budget/controller/BudgBimokController.java
+++ erp47/src/main/java/com/seoulit/erp47/acc/budget/controller/BudgBimokController.java
@@ -1,7 +1,6 @@
 package com.seoulit.erp47.acc.budget.controller;
 
 import java.util.List;
-
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,5 +31,15 @@ public class BudgBimokController {
 			Map<String, String> argsMap = dataSetBeanMapper.variablesToMap(inData);
 			List<BudgBimokBean> bimokList = budgetServiceFacade.findBimokList(argsMap);
 			dataSetBeanMapper.beansToDataset(outData, bimokList, BudgBimokBean.class);
+		}
+		
+		// 예산비목 전년도복사 
+		@RequestMapping("acc/budget/copyBimok.do")
+		public void copyBimok(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		    PlatformData inData = (PlatformData) request.getAttribute("inData");
+			PlatformData outData = (PlatformData) request.getAttribute("outData");
+			String year = inData.getVariable("year").getString();
+			List<BudgBimokBean> budgBimokList = budgetServiceFacade.copyBimok(year);
+			dataSetBeanMapper.beansToDataset(outData, budgBimokList, BudgBimokBean.class);
 		}
 }
