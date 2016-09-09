d21ac986e792af2b0b6396066e7ac4df01a194c6
mickey <rlagudtjq586@naver.com>
2016-09-09 17:51:19 +0900

    mickey

---
+++ erp47/src/main/java/com/seoulit/erp47/log/BindLogCdBean.java
@@ -0,0 +1,32 @@
+package com.seoulit.erp47.log;
+
+import com.seoulit.erp47.common.annotation.Dataset;
+import com.seoulit.erp47.common.to.BaseBean;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @Package  com.seoulit.erp47.log.base.to
+ * @Class    BindLogCdBean.java
+ * @Create   2016. 08. 26.
+ * @Author   godseop
+ * @Description 코드바인드
+ *
+ * @LastUpdated 
+ */
+@Getter
+@Setter
+@ToString
+@Dataset(name = "dsBindLogCd")
+public class BindLogCdBean extends BaseBean {
+
+    private String cdDiv,       // 코드구분
+                   code,        // 코드
+                   secCode,     // 검색조건 코드1
+                   thrCode,     // 검색조건 코드2
+                   cdNm,        // 코드명
+                   dsLogName;   // 데이터셋명
+    
+}
