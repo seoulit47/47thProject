50d3605a0fa155ae08a97e6840a4f14aa5a11b4a
mickey <rlagudtjq586@naver.com>
2016-09-19 21:30:20 +0900

    mickey 우편번호팝업완료,거래처크루드내일완료됨,코드팝업폼완료

--- erp47/src/main/java/com/seoulit/erp47/acc/elementary/exception/AcntCopyException.java
+++
@@ -1,8 +0,0 @@
-package com.seoulit.erp47.acc.elementary.exception;
-
-@SuppressWarnings("serial")
-public class AcntCopyException extends Exception{
-    public AcntCopyException(String errorMsg){
-        super(errorMsg);
-    }
-}
