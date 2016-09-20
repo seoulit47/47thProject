98f533dbb447c6e47f718130e47432521525b0cc
mickey <rlagudtjq586@naver.com>
2016-09-20 21:02:06 +0900

    mickey

---
+++ erp47/src/main/java/com/seoulit/erp47/log/inpt/applicationService/SkillExmntapplicationServiceFacade.java
@@ -0,0 +1,14 @@
+package com.seoulit.erp47.log.inpt.applicationService;
+
+import java.util.List;
+import java.util.Map;
+
+import com.seoulit.erp47.log.inpt.to.SkillExmntHistBean;
+
+public interface SkillExmntapplicationServiceFacade {
+
+	
+	
+			public List<SkillExmntHistBean>skillexmntList(Map<String, String>argsMap);
+	
+}
---
+++ erp47/src/main/java/com/seoulit/erp47/log/inpt/dao/InptDAO.java
@@ -0,0 +1,11 @@
+package com.seoulit.erp47.log.inpt.dao;
+
+import java.util.List;
+
+import com.seoulit.erp47.log.inpt.to.CtrtInfoBean;
+
+public interface InptDAO {
+
+	
+	List<CtrtInfoBean>findCtrtList();
+}
