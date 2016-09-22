bbb80b99546d02bc9fb655773599b163fa8914dd
seoulit47 <47members@naver.com>
2016-09-22 21:13:57 +0900

    2016/09/22 Sooin 자체커밋

--- erp47/src/main/java/com/seoulit/erp47/acc/budget/applicationService/AccBudgetApplicationService.java
+++ erp47/src/main/java/com/seoulit/erp47/acc/budget/applicationService/AccBudgetApplicationService.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 import com.seoulit.erp47.acc.budget.exception.BimokCopyException;
 import com.seoulit.erp47.acc.budget.to.BudgBimokBean;
-import com.seoulit.erp47.acc.budget.to.BudgRegBean;
 import com.seoulit.erp47.acc.budget.to.BudgUseDeptBean;
+import com.seoulit.erp47.acc.budget.to.RunBudgBean;
 
 public interface AccBudgetApplicationService {
 
@@ -19,5 +19,7 @@ public interface AccBudgetApplicationService {
 	List<BudgUseDeptBean> findBudgUseDept(Map<String, String> argsMap);
 	
 	void batchBudgUseDeptProcess(List<BudgUseDeptBean> useDeptBeanList);
+	
+	List<RunBudgBean> findRunBudg(Map<String, String> argsMap);
 	
 }
